df0e9ff9c2ac90e3892f8ea53ddf1076f24172cb
Sakthi <87663226+Sakthiiii@users.noreply.github.com>
2022-09-20 15:10:30 +0530

    first

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -52,9 +52,9 @@ const Contact = () => {
         phone: "",
       });
       window.location.reload();
-      console.log(setFormData)
+   
     } catch (err) {
-      console.error("error");
+      console.error("error", err.responce.data);
     }
   };
 
